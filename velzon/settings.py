28787193d5ea4b7f901d2fe596f3dd9de87d9eab
Fayz Sarfraz <fayz475@gmail.com>
2024-02-07 19:30:36 +0500

    changes made to check in settings.py file

--- velzon/settings.py
+++ velzon/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 # import ldap
+# theasdfajksdfjklasfkljasfhaklsjfaklsfhakfjhf
 
 import logging
 
