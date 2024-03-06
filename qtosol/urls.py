231e2e32c6d9e1eb0a7bbdc07a6c7633f24fbce6
Fayz Sarfraz <fayz475@gmail.com>
2024-03-07 01:10:43 +0500

    Login Functionality

--- qtosol/urls.py
+++ qtosol/urls.py
@@ -7,9 +7,17 @@ from qtosol.views import(
     qtosol_sample_view,
     qtosol_quote_view,
     qtosol_career_view,
+    qtosol_Register_view,
+
+    qtosol_blog_view,
+    qtosol_CheckOut_view,
+    qtosol_Cart_view,
+    qtosol_Projects_view,
+
 
 
 )
+from . import views
 
 urlpatterns = [
     path("", view=qtosol_home_view, name="qtosolhome"),
@@ -19,6 +27,19 @@ urlpatterns = [
     path("ContactUs", view=qtosol_contactus_view, name="qtosolcontact"),
     path("Sample", view=qtosol_sample_view, name="qtosolsample"),
     path("quote", view=qtosol_quote_view, name="qtosolquote"),
+    path("Register", view=qtosol_Register_view, name="qtosolRegister"),
+    
+    path("blog", view=qtosol_blog_view, name="qtosolblog"),
+    path("Cart", view=qtosol_Cart_view, name="qtosolCart"),
+    path("CheckOut", view=qtosol_CheckOut_view, name="qtosolCheckOut"),
+    path("Projects", view=qtosol_Projects_view, name="qtosolProjects"),
+    path('project_detail/<int:pk>/', views.project_detail_view, name='project_detail_view'),
+
+
+
+    # path('login/', QtosolLoginView.as_view(), name='login'),
+    path('Login/', views.login_view, name='login'),
+    path('Logout/', views.logout_view, name='logout'),
 
     # # Horizontal
     # path("horizontal", view=layouts_horizontal_view, name="horizontal"),
--- qtosol/views.py
+++ qtosol/views.py
@@ -1,8 +1,13 @@
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import TemplateView
-
-
+from django.shortcuts import redirect
+from allauth.account.views import LoginView
+from django.contrib.auth import login,logout
+from django.views.generic import FormView
+from django.contrib.auth.forms import AuthenticationForm
+from apps.models import Project
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 class qtosolView(TemplateView):
@@ -10,10 +15,58 @@ class qtosolView(TemplateView):
 
 
 
-qtosol_home_view = qtosolView.as_view(template_name="qtosol/home.html")
+# qtosol_home_view = qtosolView.as_view(template_name="qtosol/home.html")
+def qtosol_home_view(request):
+    # Assuming you have a User model and the user is authenticated
+    user = request.user
+    return render(request, 'qtosol/home.html', {'user': user})
 qtosol_services_view = qtosolView.as_view(template_name="qtosol/Service.html")
 qtosol_contactus_view = qtosolView.as_view(template_name="qtosol/ContactUs.html")
 qtosol_sample_view = qtosolView.as_view(template_name="qtosol/Sample.html")
 qtosol_quote_view = qtosolView.as_view(template_name="qtosol/Quote.html")
 qtosol_aboutus_view = qtosolView.as_view(template_name="qtosol/AboutUs.html")
 qtosol_career_view = qtosolView.as_view(template_name="qtosol/Career.html")
+qtosol_Register_view = qtosolView.as_view(template_name="qtosol/Register.html")
+
+# qtosol_login_view = qtosolView.as_view(template_name="qtosol/Login.html")
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            # Redirect to the home page
+            return redirect('/')
+    else:
+        form = AuthenticationForm()
+    
+    return render(request, 'qtosol/Login.html', {'form': form})
+def logout_view(request):
+    logout(request)
+    return redirect('/')
+
+
+
+
+
+
+
+
+
+qtosol_blog_view = qtosolView.as_view(template_name="qtosol/Blog.html")
+qtosol_CheckOut_view = qtosolView.as_view(template_name="qtosol/CheckOut.html")
+qtosol_Cart_view = qtosolView.as_view(template_name="qtosol/Cart.html")
+# qtosol_Projects_view = qtosolView.as_view(template_name="qtosol/Projects.html")
+# views.py in the app where you want to display projects
+@login_required
+def qtosol_Projects_view(request):
+    projects = Project.objects.all().order_by('-project_id')
+    return render(request, "qtosol/Projects.html", {'projects': projects})
+@login_required
+def project_detail_view(request, pk):
+    projects = get_object_or_404(Project, project_id=pk)
+    return render(request, "qtosol/ProjectDetail.html", {'projects': projects})
+
+
+
