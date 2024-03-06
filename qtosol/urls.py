3391ddb7debb05621f05565ec7f9ee8e480d2b06
Fayz Sarfraz <fayz475@gmail.com>
2024-03-06 20:28:48 +0500

    React pages added in Git

--- qtosol/urls.py
+++ qtosol/urls.py
@@ -1,10 +1,24 @@
 from django.urls import path
 from qtosol.views import(
     qtosol_home_view,
+    qtosol_aboutus_view,
+    qtosol_services_view,
+    qtosol_contactus_view,
+    qtosol_sample_view,
+    qtosol_quote_view,
+    qtosol_career_view,
+
+
 )
 
 urlpatterns = [
     path("", view=qtosol_home_view, name="qtosolhome"),
+    path("AboutUs", view=qtosol_aboutus_view, name="qtosolabout"),
+    path("Services", view=qtosol_services_view, name="qtosolservices"),
+    path("career", view=qtosol_career_view, name="qtosolcareer"),
+    path("ContactUs", view=qtosol_contactus_view, name="qtosolcontact"),
+    path("Sample", view=qtosol_sample_view, name="qtosolsample"),
+    path("quote", view=qtosol_quote_view, name="qtosolquote"),
 
     # # Horizontal
     # path("horizontal", view=layouts_horizontal_view, name="horizontal"),
--- qtosol/views.py
+++ qtosol/views.py
@@ -11,3 +11,9 @@ class qtosolView(TemplateView):
 
 
 qtosol_home_view = qtosolView.as_view(template_name="qtosol/home.html")
+qtosol_services_view = qtosolView.as_view(template_name="qtosol/Service.html")
+qtosol_contactus_view = qtosolView.as_view(template_name="qtosol/ContactUs.html")
+qtosol_sample_view = qtosolView.as_view(template_name="qtosol/Sample.html")
+qtosol_quote_view = qtosolView.as_view(template_name="qtosol/Quote.html")
+qtosol_aboutus_view = qtosolView.as_view(template_name="qtosol/AboutUs.html")
+qtosol_career_view = qtosolView.as_view(template_name="qtosol/Career.html")
